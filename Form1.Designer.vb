6751f975b726e960e9856806adab8d262adffade
Jackson Woodside <54115088+jdwoo090@users.noreply.github.com>
2020-03-09 20:45:48 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ticTacToeForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Box1 = New System.Windows.Forms.Label()
+        Me.Box9 = New System.Windows.Forms.Label()
+        Me.Box8 = New System.Windows.Forms.Label()
+        Me.Box6 = New System.Windows.Forms.Label()
+        Me.Box5 = New System.Windows.Forms.Label()
+        Me.Box4 = New System.Windows.Forms.Label()
+        Me.Box3 = New System.Windows.Forms.Label()
+        Me.Box2 = New System.Windows.Forms.Label()
+        Me.Box7 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.playerLabel1 = New System.Windows.Forms.Label()
+        Me.playerLabel2 = New System.Windows.Forms.Label()
+        Me.playerAnswerScoreLabel2 = New System.Windows.Forms.Label()
+        Me.playerAnswerScoreLabel1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Box1
+        '
+        Me.Box1.AutoSize = True
+        Me.Box1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box1.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box1.Location = New System.Drawing.Point(33, 61)
+        Me.Box1.Name = "Box1"
+        Me.Box1.Size = New System.Drawing.Size(30, 33)
+        Me.Box1.TabIndex = 0
+        Me.Box1.Text = "  "
+        '
+        'Box9
+        '
+        Me.Box9.AutoSize = True
+        Me.Box9.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box9.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box9.Location = New System.Drawing.Point(150, 155)
+        Me.Box9.Name = "Box9"
+        Me.Box9.Size = New System.Drawing.Size(30, 33)
+        Me.Box9.TabIndex = 1
+        Me.Box9.Text = "  "
+        '
+        'Box8
+        '
+        Me.Box8.AutoSize = True
+        Me.Box8.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box8.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box8.Location = New System.Drawing.Point(91, 155)
+        Me.Box8.Name = "Box8"
+        Me.Box8.Size = New System.Drawing.Size(30, 33)
+        Me.Box8.TabIndex = 2
+        Me.Box8.Text = "  "
+        '
+        'Box6
+        '
+        Me.Box6.AutoSize = True
+        Me.Box6.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box6.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box6.Location = New System.Drawing.Point(150, 106)
+        Me.Box6.Name = "Box6"
+        Me.Box6.Size = New System.Drawing.Size(30, 33)
+        Me.Box6.TabIndex = 3
+        Me.Box6.Text = "  "
+        '
+        'Box5
+        '
+        Me.Box5.AutoSize = True
+        Me.Box5.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box5.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box5.Location = New System.Drawing.Point(91, 106)
+        Me.Box5.Name = "Box5"
+        Me.Box5.Size = New System.Drawing.Size(30, 33)
+        Me.Box5.TabIndex = 4
+        Me.Box5.Text = "  "
+        '
+        'Box4
+        '
+        Me.Box4.AutoSize = True
+        Me.Box4.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box4.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box4.Location = New System.Drawing.Point(33, 106)
+        Me.Box4.Name = "Box4"
+        Me.Box4.Size = New System.Drawing.Size(30, 33)
+        Me.Box4.TabIndex = 5
+        Me.Box4.Text = "  "
+        '
+        'Box3
+        '
+        Me.Box3.AutoSize = True
+        Me.Box3.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box3.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box3.Location = New System.Drawing.Point(150, 61)
+        Me.Box3.Name = "Box3"
+        Me.Box3.Size = New System.Drawing.Size(30, 33)
+        Me.Box3.TabIndex = 6
+        Me.Box3.Text = "  "
+        '
+        'Box2
+        '
+        Me.Box2.AutoSize = True
+        Me.Box2.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box2.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box2.Location = New System.Drawing.Point(91, 61)
+        Me.Box2.Name = "Box2"
+        Me.Box2.Size = New System.Drawing.Size(30, 33)
+        Me.Box2.TabIndex = 7
+        Me.Box2.Text = "  "
+        '
+        'Box7
+        '
+        Me.Box7.AutoSize = True
+        Me.Box7.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Box7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Box7.Font = New System.Drawing.Font("Baskerville Old Face", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Box7.Location = New System.Drawing.Point(33, 155)
+        Me.Box7.Name = "Box7"
+        Me.Box7.Size = New System.Drawing.Size(30, 33)
+        Me.Box7.TabIndex = 8
+        Me.Box7.Text = "  "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(47, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 25)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Tic Tac Toe"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(128, 259)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(128, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Reset Board"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'playerLabel1
+        '
+        Me.playerLabel1.AutoSize = True
+        Me.playerLabel1.Location = New System.Drawing.Point(12, 210)
+        Me.playerLabel1.Name = "playerLabel1"
+        Me.playerLabel1.Size = New System.Drawing.Size(48, 13)
+        Me.playerLabel1.TabIndex = 12
+        Me.playerLabel1.Text = "Player 1:"
+        '
+        'playerLabel2
+        '
+        Me.playerLabel2.AutoSize = True
+        Me.playerLabel2.Location = New System.Drawing.Point(12, 250)
+        Me.playerLabel2.Name = "playerLabel2"
+        Me.playerLabel2.Size = New System.Drawing.Size(48, 13)
+        Me.playerLabel2.TabIndex = 13
+        Me.playerLabel2.Text = "Player 2:"
+        '
+        'playerAnswerScoreLabel2
+        '
+        Me.playerAnswerScoreLabel2.AutoSize = True
+        Me.playerAnswerScoreLabel2.Location = New System.Drawing.Point(73, 250)
+        Me.playerAnswerScoreLabel2.Name = "playerAnswerScoreLabel2"
+        Me.playerAnswerScoreLabel2.Size = New System.Drawing.Size(0, 13)
+        Me.playerAnswerScoreLabel2.TabIndex = 14
+        '
+        'playerAnswerScoreLabel1
+        '
+        Me.playerAnswerScoreLabel1.AutoSize = True
+        Me.playerAnswerScoreLabel1.Location = New System.Drawing.Point(73, 210)
+        Me.playerAnswerScoreLabel1.Name = "playerAnswerScoreLabel1"
+        Me.playerAnswerScoreLabel1.Size = New System.Drawing.Size(0, 13)
+        Me.playerAnswerScoreLabel1.TabIndex = 15
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(128, 229)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Reset Score"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ticTacToeForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(215, 284)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.playerAnswerScoreLabel1)
+        Me.Controls.Add(Me.playerAnswerScoreLabel2)
+        Me.Controls.Add(Me.playerLabel2)
+        Me.Controls.Add(Me.playerLabel1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Box7)
+        Me.Controls.Add(Me.Box2)
+        Me.Controls.Add(Me.Box3)
+        Me.Controls.Add(Me.Box4)
+        Me.Controls.Add(Me.Box5)
+        Me.Controls.Add(Me.Box6)
+        Me.Controls.Add(Me.Box8)
+        Me.Controls.Add(Me.Box9)
+        Me.Controls.Add(Me.Box1)
+        Me.Name = "ticTacToeForm"
+        Me.Text = "Tic Tac Toe"
+        Me.TransparencyKey = System.Drawing.Color.Snow
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Box1 As Label
+    Friend WithEvents Box9 As Label
+    Friend WithEvents Box8 As Label
+    Friend WithEvents Box6 As Label
+    Friend WithEvents Box5 As Label
+    Friend WithEvents Box4 As Label
+    Friend WithEvents Box3 As Label
+    Friend WithEvents Box2 As Label
+    Friend WithEvents Box7 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents playerLabel1 As Label
+    Friend WithEvents playerLabel2 As Label
+    Friend WithEvents playerAnswerScoreLabel2 As Label
+    Friend WithEvents playerAnswerScoreLabel1 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class ticTacToeForm
+    Dim player As String = "X"
+    'this is code
+    Dim player1score As Integer
+    Dim player2score As Integer
+
+    Dim gameover As Boolean
+
+    Sub resetscore()
+        player1score = 0
+        player2score = 0
+        player = "X"
+    End Sub
+
+    Sub resetboard()
+        Box1.Text = "  "
+        Box2.Text = "  "
+        Box3.Text = "  "
+        Box4.Text = "  "
+        Box5.Text = "  "
+        Box6.Text = "  "
+        Box7.Text = "  "
+        Box8.Text = "  "
+        Box9.Text = "  "
+        Me.BackColor = Color.White
+    End Sub
+
+    Sub checkwinner()
+        If Box1.Text = Box2.Text And Box2.Text = Box3.Text And Box1.Text <> "  " Or
+           Box1.Text = Box4.Text And Box4.Text = Box7.Text And Box1.Text <> "  " Or
+           Box1.Text = Box5.Text And Box5.Text = Box9.Text And Box1.Text <> "  " Or
+           Box2.Text = Box5.Text And Box5.Text = Box8.Text And Box2.Text <> "  " Or
+           Box3.Text = Box6.Text And Box6.Text = Box9.Text And Box3.Text <> "  " Or
+           Box3.Text = Box5.Text And Box5.Text = Box7.Text And Box3.Text <> "  " Or
+           Box4.Text = Box5.Text And Box5.Text = Box6.Text And Box4.Text <> "  " Or
+           Box7.Text = Box8.Text And Box8.Text = Box9.Text And Box7.Text <> "  " Then
+            Me.BackColor = Color.Green
+
+            gameover = True
+            If player = "O" Then
+                player1score = player1score + 1
+                playerAnswerScoreLabel1.Text = player1score
+            Else
+                player2score = player2score + 1
+                playerAnswerScoreLabel2.Text = player2score
+            End If
+            ' Else
+            '   Me.BackColor = Color.Red (removed due to screen being red every time it inputs a letter)
+        End If
+        ' этот текст
+        ' здравствуйте моя дети!!
+    End Sub
+    Private Sub Box1_Click(sender As Object, e As EventArgs) Handles Box1.Click, Box2.Click, Box3.Click, Box4.Click, Box5.Click, Box6.Click, Box7.Click, Box8.Click, Box9.Click
+        If sender.text = "  " And Not gameover Then
+            sender.text = player
+            If player = "X" Then
+                player = "O"
+            Else
+                player = "X"
+            End If
+            checkwinner()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        resetboard()
+
+        ' playerAnswerScoreLabel1.Text = " " (moved to new button for efficiency)
+        ' playerAnswerScoreLabel2.Text = " " (moved to new button for efficiency)
+
+        gameover = False
+        ' resetscore() (moved to new button for efficiency)
+        ' player1score = 0 (removed due to easier method being found)
+        ' player2score = 0 (removed due to easier method being found)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        ' (everything below removed for being too complicated, and was replaced with easier method)
+        ' If Box1.Text = Box2.Text And
+        ' Box2.Text = Box3.Text And
+        ' Box1.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' ElseIf Box1.Text = Box5.Text And
+        ' Box5.Text = Box9.Text And
+        ' Box1.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' ElseIf Box3.Text = Box5.Text And
+        ' Box5.Text = Box7.Text And
+        ' Box3.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' ElseIf Box1.Text = Box4.Text And
+        ' Box4.Text = Box7.Text And
+        ' Box1.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' ElseIf Box2.Text = Box5.Text And
+        ' Box5.Text = Box8.Text And
+        ' Box2.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' ElseIf Box3.Text = Box6.Text And
+        ' Box6.Text = Box9.Text And
+        ' Box3.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' ElseIf Box4.Text = Box5.Text And
+        ' Box5.Text = Box6.Text And
+        ' Box4.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' ElseIf Box7.Text = Box8.Text And
+        ' Box8.Text = Box9.Text And
+        ' Box7.Text <> "  " Then
+        ' Me.BackColor = Color.Green
+        ' Else
+        ' Me.BackColor = Color.Red
+        ' End If
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        playerAnswerScoreLabel1.Text = " "
+        playerAnswerScoreLabel2.Text = " "
+        resetscore()
+    End Sub
+End Class
